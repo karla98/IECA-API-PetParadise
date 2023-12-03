d527dda172cb71252c4c412a6fd3062f316d91a2
karla_rios <riosdiazkm@gmail.com>
2023-12-02 18:48:34 -0600

    Subir imagen de perfil

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ router.use('/mascotas', require('./mascotas'));
 router.use('/especies', require('./especies'));
 router.use('/razas', require('./razas'));
 router.use('/usuarios', require('./usuarios'));
+router.use('/perfil', require('./perfil'));
 router.use('/eventos', require('./eventos'));
 
 
---
+++ routes/perfil.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+
+const Usuario = require("../models/usuarios.model");
+
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const cloudinary = require("cloudinary").v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
+const streamifier = require("streamifier");
+
+const authMiddleware = require("../middlewares/auth");
+
+//Subir imagen de perfil
+router.post("/", authMiddleware, upload.single("imagen"),  async (req, res) => {
+    try {
+      console.log("req.files: ", req.file);
+
+      const usuarioId = req.usuario.usuario_id;
+
+      const imagen = req.file;
+
+      if (!imagen) {
+        return res.status(400).json({ error: "Por favor, envía una imagen" });
+      }
+
+      const imagenUrl = await new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result.secure_url);
+          }
+        );
+  
+        streamifier.createReadStream(imagen.buffer).pipe(stream);
+      });
+
+      const usuario = await Usuario.findByIdAndUpdate(
+        usuarioId,
+        { imagen: imagenUrl },
+        {
+          new: true,
+        }
+      );
+
+      res.json(usuario);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  }
+);
+
+module.exports = router;
